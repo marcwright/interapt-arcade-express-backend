7d739a71a81d845c629d51a09cef10078fda69f6
Marc Wright <marc.wright@gmail.com>
2019-11-11 20:35:20 -0500

    Add api/users fnidall

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 // It's very crucial that the file name matches the name attribute in your html
 app.post('/upload', upload.single('myImage'), (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models').User
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  User.findAll()
+    .then(users => {
+      res.json({ users })
+    })
 });
 
 module.exports = router;
