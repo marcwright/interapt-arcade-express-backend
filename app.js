31b79897d0264de8831ea30f8bffa3faf517a573
Marc Wright <marc.wright@gmail.com>
2019-11-11 19:23:40 -0500

    Update s3 bucket process env again

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get('/sign-s3', (req, res) => {
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
     // SEQUELIZE returnData.url
     res.json(returnData);
